a5df3c92dc23026c3a555424f5a347fae32c58ec
bretoreta <bremmurd08@gmail.com>
2023-02-24 06:13:37 +0300

    Feature:Add SEO Texts To Header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,25 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Primary Meta Tags -->
+        <title>Storm Sacco - Sacco Management System</title>
+        <meta name="title" content="Storm Sacco - Sacco Management System">
+        <meta name="description" content="A great tool to help manage and streamline the operations of any microfinance Sacco.">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://metatags.io/">
+        <meta property="og:title" content="Storm Sacco - Sacco Management System">
+        <meta property="og:description" content="A great tool to help manage and streamline the operations of any microfinance Sacco.">
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="https://metatags.io/">
+        <meta property="twitter:title" content="Storm Sacco - Sacco Management System">
+        <meta property="twitter:description" content="A great tool to help manage and streamline the operations of any microfinance Sacco.">
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
