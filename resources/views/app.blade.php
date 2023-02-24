e8eb96fd890b91ca22148b1fd44da0e2e35ab383
bretoreta <bremmurd08@gmail.com>
2023-02-24 06:23:54 +0300

    Bug:Fix Meta Tags Showing Wrong Image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,17 +11,17 @@
 
         <!-- Open Graph / Facebook -->
         <meta property="og:type" content="website">
-        <meta property="og:url" content="https://metatags.io/">
+        <meta property="og:url" content="https://sacco.infinitylabs.africa/">
         <meta property="og:title" content="Storm Sacco - Sacco Management System">
         <meta property="og:description" content="A great tool to help manage and streamline the operations of any microfinance Sacco.">
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+        <meta property="og:image" content="https://sacco.infinitylabs.africa/img/logoAsset2@4x.png">
 
         <!-- Twitter -->
         <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://metatags.io/">
+        <meta property="twitter:url" content="https://sacco.infinitylabs.africa/">
         <meta property="twitter:title" content="Storm Sacco - Sacco Management System">
         <meta property="twitter:description" content="A great tool to help manage and streamline the operations of any microfinance Sacco.">
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+        <meta property="twitter:image" content="https://sacco.infinitylabs.africa/img/logoAsset2@4x.png">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
